255b195f196663d297d5f48859fe51e0cce39fe6
Emin-hub <eminhuseynov.82@gmail.com>
2019-10-03 19:42:04 +0400

    first commit

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    
+
+    // $('#click-button').on('click', function() {
+        $('#test').css('color', 'red').text('changed');
+    // })
+    // setInterval(function() {
+    //     $('#test').css('color', 'red').text(Date());
+    // }, 1000)
+
+    // setTimeout(function() {
+    //     $('#click-button').text('deyisdi')
+    // }, 5000)
+
+    // setTimeout(function() {
+    //     var text = $('#click-button').text();
+
+    //     console.log(text)
+        
+    //     $('#click-button').text('deyisdi')
+
+    //     setTimeout(function() {
+    //         $('#click-button').text('aaaaa')
+    //     }, 1000)
+    // }, 2000)
+
+
+    // $('#click-button').on('click',function() {
+    //     // $('#test').hide()
+    //     $('#test').toggle();
+    // })
+
+       
+
+});
